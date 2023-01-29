136c58dd22c3873de13f1ab2dac95a6f4f0ec08d
ilkkajos <114188986+ilkkajos@users.noreply.github.com>
2023-01-28 20:54:20 -0500

    Update CampFin-Front.py

--- CampFin-Front.py
+++ CampFin-Front.py
@@ -7,6 +7,9 @@ issues=["abortion","gun_control", "climate_change", "gender_identity", "pro_mari
 checked=[]
 
 DATA_URL='https://raw.githubusercontent.com/ramseybe/hackathon_campaign/main/50_toss_up1.csv'
+
+left_column, right_column = st.columns([2,5])
+
 @st.cache
 def load_data(file):
     data = pd.read_csv(file)
@@ -22,28 +25,16 @@ for key, val in thing.items():
 
 def checkbox_container(data):
     st.subheader('Check the important issues to you:')
-    cols = st.columns(10)
-    if cols[1].button('Select All'):
-        for i in data:
-            st.session_state['dynamic_checkbox_' + i] = True
-        st.experimental_rerun()
-    if cols[2].button('UnSelect All'):
-        for i in data:
-            st.session_state['dynamic_checkbox_' + i] = False
-        st.experimental_rerun()
     for i in data:
         st.checkbox(i, key='dynamic_checkbox_' + i)
 
 def get_selected_checkboxes():
     return [i.replace('dynamic_checkbox_','') for i in st.session_state.keys() if i.startswith('dynamic_checkbox_') and st.session_state[i]]
 
-
-checkbox_container(issues)
-st.write('You selected:')
-st.write(get_selected_checkboxes())
+with left_column:
+    checkbox_container(issues)
+    st.write('You selected:')
+    st.write(get_selected_checkboxes())
 
 checked = get_selected_checkboxes()
-
-candidates=[]
-print(thing)
 
